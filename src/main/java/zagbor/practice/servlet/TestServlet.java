83727938a65f126719c48107e32b46cc50017cb5
zagbor <zagbor@gmail.com>
2021-02-04 19:47:39 +0300

    test

--- src/main/java/zagbor/practice/servlet/TestServlet.java
+++ src/main/java/zagbor/practice/servlet/TestServlet.java
@@ -6,16 +6,19 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 @WebServlet(
-        name = "LoginUser",
-        description = "Вход пользователя - проверка имени пользователя и пароля",
-        urlPatterns = "/LoginUser"
+
+        urlPatterns = "/name"
 )
 public class TestServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-      
+        response.setContentType("text/html;charset=utf-8");
+        PrintWriter out = response.getWriter();
+        out.println("Привет");
+        out.close();
     }
 }
